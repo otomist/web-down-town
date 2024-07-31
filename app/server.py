6d76c767f1f1f7d30156afac07da55c3bf13651e
otomist <13265059+otomist@users.noreply.github.com>
2024-07-31 11:00:14 -0700

    simple template that loads html text and serves htmx.min.js

--- app/server.py
+++ app/server.py
@@ -1,12 +1,18 @@
 # uvicorn server:app --port=8111 --reload --log-level debug
 print("hello web")
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
 
 @app.get("/", response_class=HTMLResponse)
-async def read_root():
-    return "<h1>Hello World</h1>"
+async def root(request: Request):
+    return templates.TemplateResponse(request=request, name="main.html")
