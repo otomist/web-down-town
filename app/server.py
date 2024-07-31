df9646d2a5679b0c4c7d06a224b93351beb5cd0b
otomist <13265059+otomist@users.noreply.github.com>
2024-07-31 10:36:40 -0700

    ignore pycache

--- app/server.py
+++ app/server.py
@@ -1,2 +1,12 @@
 # uvicorn server:app --port=8111 --reload --log-level debug
 print("hello web")
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root():
+    return "<h1>Hello World</h1>"
