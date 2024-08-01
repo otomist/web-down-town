fff21a40feedbd0dc4e738afcdefe58a257b14ac
otomist <13265059+otomist@users.noreply.github.com>
2024-08-01 09:00:02 -0700

    start of discord auth flow

---
+++ app/discord-auth.py
@@ -0,0 +1,31 @@
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ENDPOINT = os.getenv("API_ENDPOINT")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+
+if not all([API_ENDPOINT, CLIENT_ID, CLIENT_SECRET, REDIRECT_URI]):
+    raise ValueError("Missing environment variables")
+
+
+def exchange_code(code):
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": REDIRECT_URI,
+    }
+    headers = {"Content-Type": "application/x-www-form-urlencoded"}
+    post_url = f"{API_ENDPOINT}/oauth2/token"
+    r = requests.post(
+        post_url,
+        data=data,
+        headers=headers,
+        auth=(CLIENT_ID, CLIENT_SECRET),  # type: ignore
+    )
+    r.raise_for_status()
+    return r.json()
